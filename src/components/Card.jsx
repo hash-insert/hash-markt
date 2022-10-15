85531ed0722c19cf37f892845f07ddf1717a5c87
kaushiksaraf <kaushiksaraf91@gmail.com>
2022-10-15 23:48:00 +0530

    chanes in homepage done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,5 @@
-import {HeartIcon} from '@heroicons/react/24/outline'
-import {StarIcon} from '@heroicons/react/24/outline'
-
+import { HeartIcon, ShoppingCartIcon } from "@heroicons/react/24/outline";
+import { StarIcon } from "@heroicons/react/24/outline";
 import "./Card.css";
 import Button from "./Button.jsx";
 import { Link } from "react-router-dom";
@@ -8,11 +7,9 @@ import { Link } from "react-router-dom";
 export default function Card(props) {
   return (
     <div className="container">
-     <div className='heart'>
-     <HeartIcon />
-     </div>
-    
-      
+      <div className="heart">
+        <HeartIcon />
+      </div>
 
       <img className="img-box" src={props.img} />
 
@@ -22,16 +19,18 @@ export default function Card(props) {
 
       <div className="rating">
         <StarIcon />
-      <StarIcon />
-       <StarIcon />
-  <StarIcon />
         <StarIcon />
-        ({props.num})
+        <StarIcon />
+        <StarIcon />
+        <StarIcon />({props.num})
       </div>
       <p>{props.desc}</p>
       <span>{props.cost}$</span>
       <Button
         name="Add To Cart"
+        className="btn"
+       
+
         handleBtn={() => {
           props.addTocart(props.prod);
         }}
