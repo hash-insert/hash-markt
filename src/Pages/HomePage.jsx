151e496fa69ba44d36d94b72de0b7dc1acfac823
kaushiksaraf <kaushiksaraf91@gmail.com>
2022-10-20 22:34:08 +0530

    catogories updated

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -13,53 +13,54 @@ import { Link } from "react-router-dom";
 
 export default function Home(props) {
   const [products, setProducts] = useState([]);
-  const [category,setCategory] = useState([])
+  const [category,setCategory] = useState("")
 
-  function handleElecBtn(){
-    console.log('electronics')
+//   function handleElecBtn(){
+//     console.log('electronics')
     
- let filterElec = products.filter((item)=> {
-  if(item.category=== 'electronics'){
-    console.log(item)
-  }
+//  let filterElec = products.filter((item)=> {
+//   if(item.category=== 'electronics'){
+//     console.log(item)
+//   }
  
- })
- return filterElec
-}
+//  })
+//  return filterElec
+// }
 
-function handleJewBtn(){
-  console.log('jewellary')
+// function handleJewBtn(){
+//   console.log('jewellary')
   
-let filterJew = products.filter((item)=> {
-if(item.category=== "jewelery"){
-  console.log(item)
-}
+// let filterJew = products.filter((item)=> {
+// if(item.category=== "jewelery"){
+//   console.log(item)
+// }
 
-})
-return filterJew
-}
-function handleMensBtn(){
-  console.log('mens cloth')
+// })
+// return filterJew
+// }
+// function handleMensBtn(){
+//   console.log('mens cloth')
   
-let filterMens = products.filter((item)=> {
-if(item.category=== "men's clothing"){
-  console.log(item)
-}
+// let filterMens = products.filter((item)=> {
+// if(item.category=== "men's clothing"){
+//   console.log(item)
+// }
 
-})
-return filterMens
-}
-function handleWomensBtn(){
-  console.log('womens cloth')
+// })
+// return filterMens
+// }
+// function handleWomensBtn(){
+//   console.log('womens cloth')
   
-let filterWomens = products.filter((item)=> {
-if(item.category=== "women's clothing"){
-  console.log(item)
-}
+// let filterWomens = products.filter((item)=> {
+// if(item.category=== "women's clothing"){
+//   console.log(item)
+ 
+// }
 
-})
-return filterWomens
-}
+// })
+//   
+// }
 
   
 
@@ -98,6 +99,19 @@ return filterWomens
     fetchCategories();
   }, []);
 
+  const filterprod = (categories) => {
+    console.log("categories ", categories)
+    setCategory(categories);
+  }
+
+  let filterCard=[];
+  if (category) {
+    filterCard = products.filter((items) => items.category === category);
+  }
+  else {
+    filterCard =products
+}
+
 
 
   // Filtering Data
@@ -111,21 +125,21 @@ return filterWomens
       <Header />
       <hr />
       <div className="tabs">
-      <Link to="/">
-      <Button name="All"  />
-      </Link>
       
-      <Button name="Electronics" handleBtn={handleElecBtn} />
-      <Button name="Jewellary" handleBtn={handleJewBtn} />
-      <Button name="Men's Clothing" handleBtn={handleMensBtn} />
-      <Button name="Women's Clothing" handleBtn={handleWomensBtn} />
-
-
+      <Button  className="all"  name="All" handleBtn={() => filterprod("")} />
+      <Button  className="all"  name="Electronics" handleBtn={() => filterprod("electronics")} />
+      <Button  className="all"  name="Jewellary" handleBtn={() => filterprod("jewelery")} />
+      <Button className="all"  name="Men's Clothing" handleBtn={() => filterprod("men's clothing")} />
+      <Button  className="all" name="Women's Clothing" handleBtn={() => filterprod("women's clothing")} />
       </div>
       
+      
       <div className="card-container">
-        {products.map((prod) => {
+
+        
+        {filterCard.map((prod) => {
           return (
+
             <Card
               key={prod.id}
               prod={prod}
